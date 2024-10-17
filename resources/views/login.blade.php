41963ec1f4fcd821b40c3e3619bb1d3607c41b7c
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-10-17 23:25:48 +0500

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+   
+</head>
+<body>
+
+    <section class="login-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="assets/images/vnexia-white-logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    
+</body>
+</html>
+
+
